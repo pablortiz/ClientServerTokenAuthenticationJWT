7b73279cf95462a4690e9650a89245cbd6227193
pablortiz <pablo.ortiz.taboada@gmail.com>
2018-03-25 11:52:55 +0100

    Add files via upload

--- app/models/mysql_clientes.php
+++ app/models/mysql_clientes.php
@@ -11,7 +11,7 @@ class Cli
 	*/	
     public function __construct() {
 
-		$this->db = new PDO('mysql:host=54.38.187.177;dbname=jwt',"root","");		
+		$this->db = new PDO('mysql:host=localhost;dbname=jwt',"root","");		
 	}
 	/*
 	*/
