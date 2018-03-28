e7a1d7a524b006e7ca7feb4175497eb894c9241c
pablortiz <pablo.ortiz.taboada@gmail.com>
2018-03-28 10:12:19 +0200

    Add files via upload

--- jwt/index.php
+++ jwt/index.php
@@ -1,9 +1,15 @@
 <?php
+require_once("auth2.php");
+$t = isset($_REQUEST["t"]) ? $_REQUEST["t"]  : "" ;
+$ret = json_decode ( Auth2::check($t) );
+$r = $ret->{'ret'};
+if (!$r) {echo "Access denied"; exit;}
+/*
+*/
 require_once 'vendor/autoload.php';
 require_once 'auth.php';
 if(!isset($_GET['p'])) die('No ha definido la página a visualizar');
 $page = strtolower($_GET['p']);
 $t = isset($_GET['t']) ?  $_GET['t']:"";
-define('APPPATH','');
 require_once "$page.php";
 ?>
