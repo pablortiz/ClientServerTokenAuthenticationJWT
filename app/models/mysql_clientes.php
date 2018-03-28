0029c84b121a442cc1167d04385c88c95c89536a
pablortiz <pablo.ortiz.taboada@gmail.com>
2018-03-28 14:32:42 +0200

    Add files via upload

---
+++ app/models/mysql_clientes.php
@@ -0,0 +1,62 @@
+<?php
+/*
+*/ 
+class Cli 
+{
+	/*
+	*/   
+    private $db;
+	private $srv;
+	/*
+	*/	
+    public function __construct() {
+
+		$this->db = new PDO('mysql:host=localhost;dbname=jwt',"root","");		
+	}
+	/*
+	*/
+    public function setCliente($n, $d, $u, $i) {
+    
+		 if (strlen($i)){
+			$sql = "UPDATE clientes SET nombre='".$n."', dni='$d' WHERE id = $i";
+		 }else{
+			$sql = "INSERT INTO clientes(nombre, dni,userid) VALUES ('" . $n . "', '" . $d . "',$u)";
+		 }
+		$result = $this->db->query($sql);
+		if ($result) {
+			return true;
+		} else {
+			return false;
+		}
+    }
+	/*
+	*/	
+	public function getClientes($u) {
+		$sql = "SELECT id, nombre, dni FROM clientes WHERE userid =".$u. " ORDER BY id DESC";
+		foreach ($this->db->query($sql) as $res) {
+			$this->srv[] = $res;
+		}
+		return $this->srv;	
+    }
+	/*
+	*/	
+	public function getClienteById($i){
+		$sql = "SELECT id, nombre, dni, userid FROM clientes WHERE id =".$i;
+		foreach ($this->db->query($sql) as $res) {
+			$this->srv[] = $res;
+		}
+		return $this->srv;	
+	}
+	/*
+	*/	
+	public function delClienteById($i){
+		$sql = "DELETE FROM clientes WHERE id =".$i;
+		$result = $this->db->query($sql);
+		if ($result) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+?>
